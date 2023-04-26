859ae637b899e98f336ac48b9f537ba528aeabb7
Thenameis2 <mihiretujackson@gmail.com>
2023-04-26 18:17:32 -0400

    added a loan calculator

--- .vs/script.js
+++ .vs/script.js
@@ -3,7 +3,7 @@ function authenticateUser(event) {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if (username === "Team3" && password === "Riches") {
-		window.location.href = "LoggedinPage.html"; // Redirect to the dashboard page
+		window.location.href = "LoggedinPage.html"; // Redirect to the LoggedinPage 
 	} else {
 		document.getElementById("error-message").innerHTML = "Invalid username or password";
 	}
@@ -11,3 +11,33 @@ function authenticateUser(event) {
 function logout() {
 	window.location.href = "login.html"; // Redirect to the login page
 }
+
+function calculateLoan() {
+    event.preventDefault();
+    const loanAmount = document.getElementById('loan-amount').value;
+    const interestRate = document.getElementById('interest-rate').value / 100 / 12;
+    const loanTerm = document.getElementById('loan-term').value * 12;
+
+    const monthlyPayment = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
+    const totalPayment = monthlyPayment * loanTerm;
+    const totalInterest = totalPayment - loanAmount;
+
+    document.getElementById('monthly-payment').value = monthlyPayment.toFixed(2);
+    document.getElementById('monthly-payment-summary').textContent = monthlyPayment.toFixed(2);
+    document.getElementById('total-payment').textContent = totalPayment.toFixed(2);
+    document.getElementById('total-interest').textContent = totalInterest.toFixed(2);
+}
+
+  
+  function resetLoan() {
+    document.getElementById("loan-amount").value = "";
+    document.getElementById("interest-rate").value = "";
+    document.getElementById("loan-term").value = "";
+    document.getElementById("monthly-payment").value = "";
+    document.getElementById("total-payment").value = "";
+    document.getElementById("total-interest").value = "";
+  
+    const loanSummaryEl = document.getElementById("loan-summary");
+    loanSummaryEl.style.display = "none";
+  }
+  
