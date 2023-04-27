fa8b87f37149c7851633f98212f4f77bfa4177c8
Isaac Watts <Isaac.s.watts@gmail.com>
2023-04-27 03:30:43 -0400

    Realtor API data read into list

--- ML.py
+++ ML.py
@@ -1,13 +1,14 @@
 import json
 
 petersburg_Houses = []
+agentsList = []
 
 with open("ZillowRes.json", 'r') as DataFile:
     data = json.load(DataFile)
     li = data['results']
     count = 0
     for element in li:
-        #NOT Petersburg,VA otherwise use the loop
+        #NOT Petersburg,VA otherwise use the loop (Zipcode too)
         if element["state"] != 'VA' or element['zipcode'] != "23803" or element['city'] != 'Petersburg': 
            continue
         count +=1
@@ -15,6 +16,40 @@ with open("ZillowRes.json", 'r') as DataFile:
             petersburg_Houses.append([count, key, value])
 
 
-for i in petersburg_Houses:
-    print(i)
+with open("Realtor.json", 'r') as AgentFile:
+    agents = json.load(AgentFile)
+    li = agents['agents']
+    for element in li:
+        for key, value in element.items():
+            agentsList.append([key, value])
+
+li = []
+listA = [] #[City, rating, email, FullName]
+flag = True
+for i in agentsList:
+    if i[0] == 'address':
+        li.append(i[1]['city'])
+    if i[0] == 'agent_rating':
+        li.append(i[1])
+    if i[0] == 'is_realtor' and i[1]:
+        flag = True
+    elif i[0]== 'is_realtor' and not i[1]:
+        flag = False
+    if i[0] == 'full_name' and flag:
+        li.append(i[1])
+    if i[0] == 'email' and flag:
+        li.append(i[1])
+    # if i[0] == 'address': #NONE OF THEM ARE PETERSBURG
+    #    for k,v in i[1].items():
+    #        print(f"{k} : {v}")
+
+i = 0
+while i < len(li) - 4:
+    listA.append([li[i],li[i+1],li[i+2], li[i+3]])
+    i += 4
+
+print(len(listA))
+print(listA)
+
+
 
