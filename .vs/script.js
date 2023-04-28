d4ff4936b1c0c2ee00a4c6ad4b961b85ee680670
Thenameis2 <mihiretujackson@gmail.com>
2023-04-27 20:50:05 -0400

    created chart

--- .vs/script.js
+++ .vs/script.js
@@ -44,9 +44,7 @@ function calculateLoan() {
     loanSummaryEl.style.display = "none";
     return false;
   }
-  function map() {
-    window.location.href = "map.html";
-  }
+ 
   fetch('AgentRes.txt')
 	.then(response => response.text())
 	.then(data => {
@@ -66,6 +64,74 @@ function calculateLoan() {
 		});
 	})
 	.catch(error => console.error(error));
+
+
+  function HousePrices() {
+    window.location.href = "HousePrices.html";
+  }
+
+  // Read the data from the HouseInfo.txt file
+fetch('HouseInfo.txt')
+.then(response => response.text())
+.then(data => {
+  // Split the data into an array of lines
+  const lines = data.split('\n');
+  // Remove any empty lines
+  const filteredLines = lines.filter(line => line.trim() !== '');
+  // Extract the address, price, and image URL from each line
+  const dataPoints = filteredLines.map(line => {
+    const [address, price, imageURL] = line.split(',');
+    return {
+      label: address,
+      data: price,
+      imageUrl: imageURL
+    };
+  });
+  // Sort the dataPoints by price in acending order
+  dataPoints.sort((a, b) => a.data - b.data);
+  // Create an array of labels and an array of data
+  const labels = dataPoints.map(dataPoint => dataPoint.label);
+  const prices = dataPoints.map(dataPoint => dataPoint.data);
+  // Create a bar chart using Chart.js
+  const ctx = document.getElementById('HouseChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'House Prices',
+        data: prices,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            const dataPoint = dataPoints[tooltipItem.index];
+            return `${dataPoint.label}: $${dataPoint.data}`;
+          },
+          afterLabel: function(tooltipItem, data) {
+            const dataPoint = dataPoints[tooltipItem.index];
+            return `<img src="${dataPoint.imageUrl}" width="200" height="150" />`;
+          }
+        }
+      }
+    }
+  });
+});
+
+
+  
   
   
 
